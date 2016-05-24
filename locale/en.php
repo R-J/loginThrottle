0282513aba5ccdf31dad3e82a8d75081f91dcdbb
r-j <robin.jurinka@gmx.net>
2016-05-24 15:47:17 +0200

    Small locale change

--- locale/en.php
+++ locale/en.php
@@ -1,5 +1,5 @@
 <?php
 
-$Definition['Too many failed login attempts.'] = 'Too many failed login attempts. Your account will be available again on %s';
+$Definition['Too many failed login attempts.'] = 'Too many failed login attempts.';
 $Definition['You have %s tries left.'] = 'You have %s tries left before your account will temporarily be suspended';
 
